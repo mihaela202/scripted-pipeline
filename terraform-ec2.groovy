264c852342bd039215f6a425394f62f24eb1d055
mihaela202 <varanita.mihaela@gmail.com>
2020-09-02 01:20:46 +0000

    commit packer

--- terraform-ec2.groovy
+++ terraform-ec2.groovy
@@ -2,7 +2,8 @@ properties([
     parameters([
         booleanParam(defaultValue: true, description: 'Do you want to run terrform apply', name: 'terraform_apply'),
         booleanParam(defaultValue: false, description: 'Do you want to run terrform destroy', name: 'terraform_destroy'),
-        choice(choices: ['dev', 'qa', 'prod'], description: '', name: 'environment')
+        choice(choices: ['dev', 'qa', 'prod'], description: '', name: 'environment'),
+        string(defaultValue: '', description: 'Provide AMI ID', name: 'ami_id', trim: false)
     ])
 ])
 def aws_region_var = ''
@@ -15,14 +16,26 @@ else if(params.environment == "qa"){
 else if(params.environment == "prod"){
     aws_region_var = "us-west-2"
 }
+def tf_vars = """
+    s3_bucket = \"jenkins-tasks\"
+    s3_folder_project = \"terraform_ec2\"
+    s3_folder_region = \"us-east-1\"
+    s3_folder_type = \"class\"
+    s3_tfstate_file = \"infrastructure.tfstate\"
+    environment = \"${params.environment}\"
+    region      = \"${aws_region_var}\"
+    public_key  = \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDU7Sg7nQZfHIAdC82fSm8CCv55wLDGVnjSfslq02jp9XWBNAW32TgdXYAhGPP661fk/ZLIKaQ3JT3/GYdev/uKsgEqCifjavHTrWg/wZ/hKffH2SMQ70PLrk7YsT43/SpUeXIJimAhdoG1OyRwAYNC/PTCsaxAfuYtfRN96dyGiEKDTjX5N4q2EQpOOawbX0htDrrr3DL++XVuCPeVyp0KRypeu34dTpHFdg+kAKHs4djMWHNEv7u0YX2ciETppCk62siiShHTv+4AuJJx0L/eX8hLbf7EXTVMQcL4M0dRtCBESf0Pu/8psm59dy9ZOT4pWI18JTi+qBut8mbJMJI1 mihaelavaranita@Mihaelas-MacBook-Pro.local\"
+    ami_id      = \"${params.ami_id}\"
+"""
 node{
     stage("Pull Repo"){
         cleanWs()
-        git url: 'https://github.com/mihaela202/terraform-ec2.git'
+        git url: 'https://github.com/ikambarov/terraform-ec2.git'
     }
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
         withEnv(["AWS_REGION=${aws_region_var}"]) {
             stage("Terrraform Init"){
+                writeFile file: "${params.environment}.tfvars", text: "${tf_vars}"
                 sh """
                     bash setenv.sh ${environment}.tfvars
                     terraform-0.13 init
